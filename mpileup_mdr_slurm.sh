cfe7706dc3bcc7bb8f9c85ab821a541be78b8bca
ko-katie <katie.ko@verizon.net>
2025-10-28 14:37:46 -0400

    Update mpileup_mdr_slurm.sh

--- mpileup_mdr_slurm.sh
+++ mpileup_mdr_slurm.sh
@@ -8,7 +8,7 @@ mkdir -p "$WORKING_DIR"
 
 # Get the sample line for the slurm array task ID
 #samples_to_run.txt is formatted as a tab-delimited file with the sample name in the first column and the path to the bam file in the second column
-SAMPLE_LINE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /path/to/mpileup_dir/samples_to_run.txt)
+SAMPLE_LINE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" samples_to_run.txt)
 
 echo "Processing sample: $SAMPLE_LINE"
 
@@ -33,7 +33,7 @@ mkdir -p "$SAMPLE_VCF_DIR"
 CONSENSUS_FASTA="${WORKING_DIR}${SAMPLE_NAME}_consensus.fasta"
 
 #run mpileup on bam file for mdr1 cds region and pipe to script which will make consensus sequence
-bcftools mpileup -Ov -r PvP01_10_v2:478539-483333 -f /path/to/PlasmoDB-67_PvivaxP01_Genome.fasta $SAMPLE_BAM | python3 /path/to/mpileup_to_fasta.py $CONSENSUS_FASTA $SAMPLE_NAME
+bcftools mpileup -Ov -r PvP01_10_v2:478539-483333 -f PlasmoDB-67_PvivaxP01_Genome.fasta $SAMPLE_BAM | python3 mpileup_to_fasta.py $CONSENSUS_FASTA $SAMPLE_NAME
 
 rm -r $SAMPLE_VCF_DIR
 
