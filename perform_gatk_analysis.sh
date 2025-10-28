6084d9ff796cd1661a5ea00fe6aa68fdb256f01e
ko-katie <katie.ko@verizon.net>
2025-10-28 14:58:54 -0400

    Update perform_gatk_analysis.sh

--- perform_gatk_analysis.sh
+++ perform_gatk_analysis.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
 
+genomicsdb_workspace_path="/path/to/genomicsdb_workspace/"
+
 #run GenomicsDBImport to combine individual VCF files listed in sample_map.txt, mask multigene families based on unmasked_regions.intervals
-python3 /path/to/packages/gatk-4.2.2.0/gatk GenomicsDBImport --sample-name-map /path/to/sample_map.txt --genomicsdb-workspace-path /path/to/fws_working_dir/genomicsdb_workspace/ --intervals /path/to/fws_working_dir/unmasked_regions.intervals --batch-size 50
+python3 gatk GenomicsDBImport --sample-name-map sample_map.txt --genomicsdb-workspace-path genomicsdb_workspace/ --intervals unmasked_regions.intervals --batch-size 50
 
 #finish combining files into all_samples_variants.output
-python3 /usr/local/packages/gatk-4.2.2.0/gatk GenotypeGVCFs -R /path/to/fws_working_dir/PlasmoDB-67_PvivaxP01_Genome.fasta -V /gendb:///path/togenomicsdb_database -O /path/to/fws_working_dir/all_samples_variants.output
+python3 gatk GenotypeGVCFs -R PlasmoDB-67_PvivaxP01_Genome.fasta -V /gendb:///${genomicsdb_workspace_path} -O all_samples_variants.vcf
 
 #run gatk Variant Filtration
-python3 /usr/local/packages/gatk-4.2.2.0/gatk VariantFiltration -R /path/to/PlasmoDB-67_PvivaxP01_Genome.fasta -V /path/to/fws_working_dir/all_samples_variants.vcf -window 35 -cluster 3 --filter-name FS -filter "FS > 60.0" --filter-name QD -filter "QD < 2.0" -O /path/to/fws_working_dir/all_samples_variants_filtered.vcf
+python3 gatk VariantFiltration -R PlasmoDB-67_PvivaxP01_Genome.fasta -V all_samples_variants.vcf -window 35 -cluster 3 --filter-name FS -filter "FS > 60.0" --filter-name QD -filter "QD < 2.0" -O all_samples_variants_filtered.vcf
 
 #run vcftools on filtered variants
-/usr/local/packages/vcftools/bin/vcftools --vcf /path/to/fws_working_dir/all_samples_variants_filtered.vcf --remove-indels --max-alleles 2 --max-missing 0.2 --recode --recode-INFO-all --out /path/to/fws_working_dir/all_samples_variants_filtered2.vcf
-
-gzip /path/to/all_samples_variants_filtered2.vcf
+vcftools --vcf all_samples_variants_filtered.vcf --remove-indels --max-alleles 2 --max-missing 0.2 --recode --recode-INFO-all --out all_samples_variants_filtered2.vcf
